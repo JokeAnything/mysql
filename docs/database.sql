042152f90158f7024b8ae2a56c9b270ad785ab95
2325082388 <2325082388@qq.com>
2018-08-18 16:27:21 +0800

    [docs]modify database.sql file.

--- docs/database.sql
+++ docs/database.sql
@@ -6,4 +6,16 @@ create database if not exists my_database charset utf8;
 --Query OK, 1 row affected (3.84 sec)
 
 --切换默认使用的数据库
-use my_database;
+use 'my_database';
+
+--显示创建数据库时的sql语句
+show create database my_database;
+
+--删除数据库，谨慎操作
+drop database my_database;
+
+--设置数据库字符集utf8
+alter database my_database character set utf8;
+
+--设置数据库字符集gbk
+alter database my_database character set gbk;
